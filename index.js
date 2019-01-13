cfb5a3c94b2cc7df6c31eef9b2c9c14a5bca072f
Marco Antonio <marcopandolfo577@gmail.com>
2019-01-13 00:44:02 -0200

    Upload

---
+++ index.js
@@ -0,0 +1,34 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const config = require('./config.json');
+
+
+//Quando o bot logar
+bot.on('ready', () => {
+    console.log(">Logado");
+
+});
+
+
+//Quando Alguem enviar mensagem
+bot.on('message', message => {
+
+    console.log("> [" + message.author.username + "] - " + message.content);
+
+    
+    if(message.author.bot) {
+        return;
+    }
+
+ 
+    var cezar = "206304601303941121";
+
+    if(message.author.id == cezar) {
+        message.channel.send("E o Cezar");
+    } else {
+        message.channel.send("Não é o cezar");
+    }
+
+});
+
+bot.login(config.token);
