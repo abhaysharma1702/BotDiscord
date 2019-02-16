b0f4d3bfa548b1225d866801d954511793517af4
Marco Antonio <marcopandolfo577@gmail.com>
2019-02-16 19:20:07 -0200

    Att

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,4 @@
 module.exports = async client => {
-    client.user.setActivity(`under maintenance`);
-    // client.user.setActivity(`Digite: ${client.config.prefix}help`);
+    client.user.setActivity(`Digite: ${client.config.prefix}help`);
     console.log(`> Bot iniciado em [${client.guilds.size}] servidores`);
 }
