0f4f199f28d7b483cb4c01999acff13ff085a00f
Marco Antonio <marcopandolfo577@gmail.com>
2019-02-21 11:37:46 -0300

    *delete aprimorado

--- commands/delete.js
+++ commands/delete.js
@@ -3,10 +3,16 @@ exports.run = async (client, message, args) => {
         message.channel.send("Por favor insira um numero :) Ex: *delete 3");
         return;
     }
-    if(parseInt(args[0]) > 20) {
+    if(parseInt(args[0]) > 100) {
         message.channel.send("Insira um numero menor");
         return;
     }
-    let number = parseInt(args[0]) + 1;
-    message.channel.fetchMessages({limit: number}).then(messages => message.channel.bulkDelete(messages));
+    let number = parseInt(args[0]);
+    message.channel.fetchMessages({limit: number}).then(messages => message.channel.bulkDelete(messages).then(
+        message.channel.send({embed: {
+            title: ":shower: Chat Limpo! :shower:",
+            description: `O chat foi limpo por ${message.author.username}`,
+            timestamp: new Date()
+        }})
+    ));
 }
