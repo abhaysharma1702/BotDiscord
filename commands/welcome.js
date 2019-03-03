41bfe797bf470d88034f1b8f5bdd6a031ca99edc
Marco Antonio Pandolfo <marcopandolfo577@gmail.com>
2019-03-02 22:28:14 -0300

    Mais bugs fixados

--- commands/welcome.js
+++ commands/welcome.js
@@ -3,9 +3,11 @@ exports.run = async (client, message, args) => {
     
     if (args[0] == null) return message.reply(`Você deve inserir o ID do canal \nEx. ${client.config.prefix}welcome 123456789123456`);
 
+    const id = args[0].replace("<", "").replace(">", "").replace("#", "");
+
     if(!message.member.hasPermission('MANAGE_GUILD')) return message.channel.send(":x: Você não tem permissão para usar esse comando");
     
     
-    message.channel.send(util.setWelcomeChat(message.member.guild, args[0]));
+    message.channel.send(util.setWelcomeChat(message.member.guild, id));
     
 };
