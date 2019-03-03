370aed041472b30289b6f022ee30842b13a9f5c0
Marco Antonio Pandolfo <marcopandolfo577@gmail.com>
2019-03-02 22:04:56 -0300

    Fixei uns bug lixo ai

--- commands/welcome.js
+++ commands/welcome.js
@@ -1,9 +1,11 @@
 const util = require('../util/util.js')
 exports.run = async (client, message, args) => {
-    if (args[0] == null) return message.reply(`Você deve inserir o ID do canal \nEx. ${client.config.preix}welcome 123456789123456`);
+    
+    if (args[0] == null) return message.reply(`Você deve inserir o ID do canal \nEx. ${client.config.prefix}welcome 123456789123456`);
 
-    if(!msg.member.hasPermission('MANAGE_GUILD')) return  message.channel.send(":x: Você não tem permissão para usar esse comando");
-
+    if(!message.member.hasPermission('MANAGE_GUILD')) return message.channel.send(":x: Você não tem permissão para usar esse comando");
+    
+    
     message.channel.send(util.setWelcomeChat(message.member.guild, args[0]));
     
 };
