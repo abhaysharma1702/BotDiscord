5bb9e79a4fa8c42d4095680bb433187b0956e22f
Marco Antonio Pandolfo <marcopandolfo577@gmail.com>
2019-03-06 00:59:12 -0300

    Consertei a merda

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,41 +1,15 @@
-const util = require('../util/util.js');
+const util = require('../util/util.js')
 module.exports = async (client, member) => {
-
-    let corDoEmbed = util.getRandonColor();
-
-    let channel_id = util.getWelcomeChat(member.guild);
-    let channel = client.channels.get(channel_id);
 
     //Mandar DM para a pessoa que entrou
     member.send({embed: {
-        color: corDoEmbed,
+        color: util.getRandonColor(),
         author: {
             name: client.username,
             icon_url: client.user.avatarURL
         },
         title: "Bem vindo ao servidor :)",
         description: "Seja bem-vindo(a) " + member.user.username + " ao nosso Servidor! \nDigite *help para ver a lista de comandos =D \nTem alguma sugestão de comando? Mande no meu discord ! <@261991713541718017>",
-        timestamp: new Date()
-    }});
-
-    //Mostrar que a pessoa entrou no sv
-    channel.send({embed: {
-        color: corDoEmbed,
-        author: {
-            name: client.user.username,
-            icon_url: client.user.avatarURL
-        },
-        title: ":tada: Bem Vindo :tada:",
-        description: `Olá ${member} Seja bem-vindo(a) ao nosso servidor !`,
-        thumbnail: {
-            url: member.user.avatarURL
-        },
-        fields: [
-            {
-                name: "😻",
-                value: "Aeeeeeeeeee!!!!"
-            }
-        ],
         timestamp: new Date()
     }});
 }
--- util/util.js
+++ util/util.js
@@ -1,10 +1,7 @@
-const db = require('sqlite-sync');
-db.connect('db/welcome.sqlite');
-
 exports.getRandonColor = () => {
     return Math.floor(Math.random() * 9999999);
 }
-exports.getDefaultChannel = (guild) => {
+exports.getDefaultChannel = guild => {
     try {
         // get "original" default channel
         if(guild.channels.has(guild.id))
@@ -23,64 +20,6 @@ exports.getDefaultChannel = (guild) => {
         Long.fromString(a.id).sub(Long.fromString(b.id)).toNumber())
         .first();
         
-    } catch (error) {
-        console.log(error);
-    }
-}
-
-exports.getWelcomeChat = guild => {
-
-    const getDefaultChannel = guild => {
-        // get "original" default channel
-        if(guild.channels.has(guild.id))
-        return guild.channels.get(guild.id)
-        
-        // Check for a "general" channel, which is often default chat
-        const generalChannel = guild.channels.find(channel => channel.name === "general" || channel.name === "geral");
-        if (generalChannel)
-        return generalChannel;
-        // Now we get into the heavy stuff: first channel in order where the bot can speak
-        // hold on to your hats!
-        return guild.channels
-        .filter(c => c.type === "text" &&
-        c.permissionsFor(guild.client.user).has("SEND_MESSAGES"))
-        .sort((a, b) => a.position - b.position ||
-        Long.fromString(a.id).sub(Long.fromString(b.id)).toNumber())
-        .first();
-    }
-
-
-    try {
-        let id = guild.id;
-        let query = "undefined";
-        db.run(`SELECT channel_id FROM welcome WHERE guild_id=${id}`, (res) => {
-            if(res.error) {
-                throw res.error;
-            }
-    
-            if(res != 'undefined' && res != null && res.length > 0){        
-                query = res[0].channel_id;
-            }
-        });
-
-        if(query == "undefined") {       
-            return getDefaultChannel(guild).toString().replace("<", "").replace(">", "").replace("#", "");
-        }
-
-        return query;
-    } catch (error) {
-        console.log(error);
-    }
-}
-exports.setWelcomeChat = (guild, id) => {
-    try {
-        const result = db.run(`INSERT INTO welcome (guild_id, channel_id) VALUES (${guild.id}, ${id})`);
-        if(result.error) {
-            db.run(`UPDATE welcome SET channel_id=${id} where guild_id=${guild.id}`);
-            return ':white_check_mark: Canal de mensagens de boas vindas atualizado com sucesso!';
-        } else {
-            return ":white_check_mark: Agora o bot irá mandar as mensagens de boas vindas nesse canal :)"
-        }
     } catch (error) {
         console.log(error);
     }
